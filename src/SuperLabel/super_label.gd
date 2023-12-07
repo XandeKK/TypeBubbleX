6970956626326fd2038d8a9d1d5a1d8960604cfd
xandekk <alexandre.dosa@gmail.com>
2023-12-07 14:04:04 -0300

    add rotation in superlabel

--- src/SuperLabel/super_label.gd
+++ src/SuperLabel/super_label.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var texture_rect : TextureRect = $TextureRect
 
 var move : Move = Move.new()
+var rotation_node : Rotation = Rotation.new()
 
 var focus : bool = false : get = _get_focus
 
@@ -12,7 +13,12 @@ signal focused(node : Control)
 
 func _ready():
 	move.target = self
+	rotation_node.target = self
+	
+	add_child(rotation_node)
+	
 	resized.connect(readjust_size)
+
 	readjust_size()
 
 func _input(event):
@@ -26,6 +32,8 @@ func _draw():
 		var bottom = text.style_box.get_margin(SIDE_BOTTOM)
 		# draw content margin rect
 		draw_rect(Rect2(Vector2(left, top), size - Vector2(right + left, bottom + top)), Color.BLUE, false, 3)
+		# draw rotation rect
+		draw_rect(Rect2(rotation_node.position, rotation_node.size), Color.RED)
 		# draw text rect
 		draw_rect(Rect2(Vector2.ZERO, size), Color.RED, false, 3)
 	else:
@@ -42,6 +50,7 @@ func readjust_size():
 	texture_rect.size = size
 	
 	pivot_offset = size / 2
+	rotation_node.readjust_size()
 	
 	text._shape()
 
@@ -52,3 +61,14 @@ func set_focus(value : bool = true, emit : bool = true) -> void:
 	focus = value
 	queue_redraw()
 	emit_signal('focused', self) if emit else null
+
+func convert_to_degrees(value : float):
+	value = int(rad_to_deg(value))
+	if value < 0:
+		value = 360 + value
+	return value
+
+func set_rotation_text(value : float):
+	value = convert_to_degrees(value)
+	rotation_degrees = value
+#	emit_signal('rotation_changed', value)
