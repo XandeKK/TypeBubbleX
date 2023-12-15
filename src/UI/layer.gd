6d250992a3d83397cc39c127c8c050844d954541
xandekk <alexandre.dosa@gmail.com>
2023-12-15 13:12:52 -0300

    fix breakline problem in layers_docker

--- src/UI/layer.gd
+++ src/UI/layer.gd
@@ -8,11 +8,11 @@ var node : Control : set = set_node
 
 func set_node(value : Control) -> void:
 	node = value
-	label.text = node.text.text
+	label.text = node.text.text.replace('\n', ' ')
 	node.text.text_changed.connect(_text_changed)
 
 func _text_changed(value : String) -> void:
-	label.text = value
+	label.text = value.replace('\n', ' ')
 
 func _on_pressed():
 	if node.focus:
