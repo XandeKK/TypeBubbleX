eda11299cc3682156ce2f41f8febb01bf082e78e
xandekk <alexandre.dosa@gmail.com>
2024-01-17 08:05:44 -0300

    finished #3

--- src/SuperLabel/Classes/gradient_text.gd
+++ src/SuperLabel/Classes/gradient_text.gd
@@ -23,28 +23,37 @@ func _set_active(value : bool) -> void:
 	
 	texture_rect.visible = active
 
-#func to_dictionary() -> Dictionary:
-	#var points : Array = []
-	#var gradient : Gradient = get_gradient()
-	#for i in gradient.get_point_count():
-		#points.append({
-			#'offset': gradient.get_offset(i),
-			#'color': gradient.get_color(i)
-		#})
-	#
-	#return {
-		#'active': active,
-		#'points': points,
-		#'fill_from': texture_rect.texture.fill_from,
-		#'fill_to': texture_rect.texture.fill_to
-	#}
-#
-#func load(value : Dictionary) -> void:
-	#active = value['active']
-	#
-	#var gradient : Gradient = get_gradient()
-	#for point in value['points']:
-		#gradient.add_point(point['offset'], point['color'])
-	#
+func to_dictionary() -> Dictionary:
+	var points : Array = []
+	var gradient : Gradient = get_gradient()
+	for i in gradient.get_point_count():
+		points.append({
+			'offset': gradient.get_offset(i),
+			'color': gradient.get_color(i)
+		})
+	
+	return {
+		'active': active,
+		'points': points,
+		'fill_from': texture_rect.texture.fill_from,
+		'fill_to': texture_rect.texture.fill_to
+	}
+
+func load(value : Dictionary) -> void:
+	active = value['active']
+	
+	var gradient : Gradient = get_gradient()
+	
+	for point in range(gradient.get_point_count() - 1, 0, -1):
+		gradient.remove_point(point)
+	
+	for point in value['points']:
+		if point == value['points'][0]:
+			gradient.set_color(0, point['color'])
+			gradient.set_offset(0, point['offset'])
+			continue
+		
+		gradient.add_point(point['offset'], point['color'])
+	
 	texture_rect.texture.fill_from = value['fill_from']
 	texture_rect.texture.fill_to = value['fill_to']
--- src/UI/Styles/gradient/gradient_2d_texture_rect.gd
+++ src/UI/Styles/gradient/gradient_2d_texture_rect.gd
@@ -12,11 +12,21 @@ func _ready():
 func clear() -> void:
 	gradient_texture_2d = null
 	texture.gradient = Gradient.new()
+	
+	var half_size : float = to_point.size.x / 2
+	from_point.position = Vector2(-half_size, -half_size)
+	to_point.position = Vector2(size.x - half_size, -half_size)
+	
+	texture.fill_from = Vector2(0, 0)
+	texture.fill_to = Vector2(1, 0)
 
 func set_gradient_text(value : GradientText) -> void:
 	gradient_texture_2d = value.get_gradient_texture_2d()
 	
 	texture.gradient = gradient_texture_2d.get_gradient()
+	
+	texture.fill_from = gradient_texture_2d.fill_from
+	texture.fill_to = gradient_texture_2d.fill_to
 	
 	from_point.position = gradient_texture_2d.fill_from * size - from_point.size / 2
 	to_point.position = gradient_texture_2d.fill_to * size - to_point.size / 2
@@ -27,9 +37,15 @@ func set_fill(value : int) -> void:
 	texture.fill = value
 
 func _on_from_point_position_changed() -> void:
+	if not gradient_texture_2d:
+		return
+	
 	gradient_texture_2d.fill_from = (from_point.position + from_point.size / 2) / size
 	texture.fill_from = gradient_texture_2d.fill_from
 
 func _on_to_point_position_changed() -> void:
+	if not gradient_texture_2d:
+		return
+	
 	gradient_texture_2d.fill_to = (to_point.position + to_point.size / 2) / size
 	texture.fill_to = gradient_texture_2d.fill_to
