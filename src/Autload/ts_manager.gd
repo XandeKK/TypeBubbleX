8b1581b42329a47ea5571d597be668ea82054bae
XandeKK <alexandre.dosa@gmail.com>
2024-10-25 05:36:23 -0300

    Delete all files

--- src/Autload/ts_manager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var TS : TextServer = TextServerManager.get_primary_interface() : get = get_text_server
-
-func get_text_server():
-	return TS
--- src/UI/Notification/notifications_box.gd
+++
@@ -1,4 +0,0 @@
-extends VBoxContainer
-
-func _ready():
-	Notification.v_box_container = self
